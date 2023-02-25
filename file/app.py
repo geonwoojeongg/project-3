24b0f991c790668a57f4b00d5d3ac40129742dc7
geonwoojeongg <andy.jeong0817@gmail.com>
2023-02-25 12:09:12 -0500

    js upload

--- file/app.py
+++ file/app.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-import os
-
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
--- file/static/js/app.js
+++ file/static/js/app.js
@@ -1,157 +1,139 @@
 const file = "/api/v1.0/health";
-let d3Data = [];
+const NOT_SATISFIED_VALUE = 1;
 
+let dropdown_gender = d3.select("#selDataset");
+let dataset_gender = dropdown_gender.property("value");
 
-// init
+let dropdown_marital = d3.select("#selDataset2");
+let dataset_marital = dropdown_marital.property("value");
+
+let dropdown_department = d3.select("#selDataset3");
+let dataset_department = dropdown_department.property("value");
+
+let dropdown_job = d3.select("#selDataset4");
+let dataset_job = dropdown_job.property("value");
+
 function init() {
-
-  let dropdownMenu = d3.select("#selDataset");
-
-  d3.json(file).then((data) => {
-
-    console.log(data)
-    // d3Data = data;
-    for (let i=0; i<=data.length; i++){
-    let gender = data[i].Gender;
-    console.log(gender)
-    }
-  })
-
-  // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
-
-// Function called by DOM changes
-function getData() {
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a letiable
-  let dataset = dropdownMenu.property("value");
-  console.log(dataset)
-  // Initialize an empty array for the country's data
-  // let data = [];
-}
-
-  // On change to the DOM, call getData()
-  d3.selectAll("#selDataset2").on("change", getData);
-
-  // Function called by DOM changes
-  function getData() {
-    let dropdownMenu = d3.select("#selDataset2");
-    // Assign the value of the dropdown menu option to a letiable
-    let dataset2 = dropdownMenu.property("value");
-    console.log(dataset2)
-    // Initialize an empty array for the country's data
-    // let data = [];
-  }
-
-    // On change to the DOM, call getData()
-    d3.selectAll("#selDataset3").on("change", getData);
-
-    // Function called by DOM changes
-    function getData() {
-      let dropdownMenu = d3.select("#selDataset3");
-      // Assign the value of the dropdown menu option to a letiable
-      let dataset3 = dropdownMenu.property("value");
-      console.log(dataset3)
-      // Initialize an empty array for the country's data
-      // let data = [];
-    }
-
-    // gender.forEach((id) => {
-    //   dropdownMenu
-    //     .append("option")
-    //     .text(id)
-    //     .property("value",id);
-    // });
-
-  //   let sample1 = names[0];
-
-  //   Metadata(sample1);
-  //   BarChart(sample1);
-  //   BubbleChart(sample1);
-  // });
+    buildgenderpieChart("Male");
+    buildbarChart("Married");
+    buildDepartmentBarChart("Cardiology");
+    buildJobSatisfactionpieChart(NOT_SATISFIED_VALUE);
 };
 
-// bar chart
-// function BarChart(sample) {
-//   let sampledata = d3Data.samples;
-//   let value = sampledata.filter(info => info.id == sample);
-//   let sampleinfo = value[0];
+function optionChangedGender(x) {
+    buildgenderpieChart(x);
+};
 
-//   let otu_ids = sampleinfo.otu_ids;
-//   let otu_labels = sampleinfo.otu_labels;
-//   let sample_values = sampleinfo.sample_values;
+function optionChangedmarital(x) {
+    buildbarChart(x);
+};
 
-//   let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-//   let xticks = sample_values.slice(0,10).reverse();
-//   let labels = otu_labels.slice(0,10).reverse();
-  
-//   let trace1 = {
-//       x: xticks,
-//       y: yticks,
-//       text: labels,
-//       type: "bar",
-//       orientation: "h"
-//   };
+function optionChangedDepartment(x) {
+    buildDepartmentBarChart(x);
+};
 
-//   let layout = {
-//       title: "Top 10 OTUs Found"
-//   };
+function optionChangedJobSatisfaction(x) {
+    buildJobSatisfactionpieChart(x);
+};
 
-//   Plotly.newPlot("bar", [trace1], layout)
-// };
+function buildgenderpieChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+    var resultArray = data.filter(sampleObj => sampleObj.Gender== x);
+    console.log(resultArray)
+    var totalGenderEmployee = resultArray.length;
+    var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+    var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+    let yes_att = yesArray.length;
+    let no_att = noArray.length;
+    let AttritionRate = yes_att / totalGenderEmployee;
+    console.log(yes_att, no_att);
 
-// // bubble chart
-// function BubbleChart(sample) {
-  
-//   let sampledata = d3Data.samples;
-//   let value = sampledata.filter(info => info.id == sample);
-//   let sampleinfo = value[0];
+    const gendertrace = [{
+        values: [AttritionRate, 1 - AttritionRate],
+        labels: ['Attrition', 'No Attrition'],
+        type: 'pie'
+    }];
 
-//   let otu_ids = sampleinfo.otu_ids;
-//   let otu_labels = sampleinfo.otu_labels;
-//   let sample_values = sampleinfo.sample_values;
+    const genderlayout = {
+        title: 'Employee Gender vs. Attrition Rates'
+    };
 
-//   let trace2 = {
-//       x: otu_ids,
-//       y: sample_values,
-//       text: otu_labels,
-//       mode: "markers",
-//       marker: {
-//           size: sample_values,
-//           color: otu_ids,
-//           colorscale: "Earth"
-//       }
-//   };
+    Plotly.newPlot('gender_pie', gendertrace, genderlayout);
+    });
+};
 
-//   let layout = {
-//       hovermode: "closest",
-//       xaxis: {title: "OTU ID"},
-//   };
+function buildbarChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+      var resultArray = data.filter(sampleObj => sampleObj.MaritalStatus== x);
+      console.log(resultArray)
+      var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+      var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+      let yes_att = yesArray.length;
+      let no_att = noArray.length;
+      console.log(yes_att, no_att)
+      var bartrace = [{
+        x: ['Yes', 'No'],
+        y: [yes_att, no_att],
+        name: 'Marital Status',
+        type: 'bar',
+        marker: {color:["green", "red"]}
+      }];
+      var barlayout = {title: 'Marital Status vs Attrition'};
+      Plotly.newPlot('marital_bar', bartrace, barlayout);
+    });
+};
 
-//   Plotly.newPlot("bubble", [trace2], layout)
-// };
+function buildDepartmentBarChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+      var resultArray = data.filter(sampleObj => sampleObj.Department== x);
+      console.log(resultArray)
+      var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+      var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+      let yes_att = yesArray.length;
+      let no_att = noArray.length;
+      console.log(yes_att, no_att)
+      var bartrace = [{
+        x: [yes_att, no_att],
+        y: ['Yes', 'No'],
+        name: 'Department',
+        type: 'bar',
+        orientation: 'h',
+        marker: {color:["green", "yellow"]}
+      }];
+      var barlayout = {
+        title: 'Department vs Attrition'};
+      Plotly.newPlot('department_bar', bartrace, barlayout);
+    });
+};
 
-// // metadata
-// function Metadata(sample) {
-
-//   let metadata = d3Data.metadata;
-//   debugger
-//   let value = metadata.filter(info => info.id == sample);
-//   let sampleinfo = value[0];
-
-//   d3.select("#sample-metadata").html("");
-
-//   Object.entries(sampleinfo).forEach(([key,value]) => {
-//       d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
-//   });
-// };
-
-// // call functions
-// function optionChanged(value) { 
-//   debugger
-//   Metadata(value);
-//   BarChart(value);
-//   BubbleChart(value);
-// };
+function buildJobSatisfactionpieChart(x){
+    d3.json(file).then((data) => {
+      console.log(data)
+    var resultArray = data.filter(sampleObj => sampleObj.JobSatisfaction== x);
+    console.log(resultArray)
+    var totalJobSatisfaction = resultArray.length;
+    var yesArray = resultArray.filter(sampleObj => sampleObj.Attrition== "Yes");
+    var noArray = resultArray.filter(sampleObj => sampleObj.Attrition== "No");
+    let yes_att = yesArray.length;
+    let no_att = noArray.length;
+    let AttritionRate = yes_att / totalJobSatisfaction;
+    console.log(yes_att, no_att);
+    const jobtrace = [{
+        values: [AttritionRate, 1 - AttritionRate],
+        labels: ["Attrition", "No Attrition"],
+        type: "pie",
+        hole: .4,
+        marker: {
+            colors:["yellow", "blue"]}
+    }];
+    const joblayout = {
+        title: "Job Satisfaction Attrition Rates"
+    };
+    Plotly.newPlot("JobSatisfaction_pie", jobtrace, joblayout);
+    });
+};
 
 init();
